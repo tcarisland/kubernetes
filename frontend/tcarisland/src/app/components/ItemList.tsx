72d112aba2ad1fd696bc0267316b482277f39950
tcarisland <tcarisland@gmail.com>
2024-02-28 12:29:43 +0100

    cleanup #19

--- frontend/tcarisland/src/app/components/ItemList.tsx
+++ frontend/tcarisland/src/app/components/ItemList.tsx
@@ -4,15 +4,6 @@ import React, {FormEvent, useEffect, useState} from "react";
 const ItemList: React.FC = ({}) => {
     const [data, setData] = useState([])
     useEffect(() => {
-        /*
-        const response = await fetch('/api/item', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json;charset=UTF-8'
-            },
-            body: JSON.stringify(requestData)
-        })
-         */
         fetch('/api/item', {
             method: 'GET'
         }).then(response => response.json()).then(data => setData(data))
